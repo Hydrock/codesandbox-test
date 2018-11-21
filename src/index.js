fa0aa8c781fadbb79b0d0b9065dcae6b430a7ec9
Alex Vechkanov <hydrock74@gmail.com>
2018-11-21 16:52:57 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// test
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Test />
+      <Test />
+    </div>
+  );
+}
+
+class Test extends Component {
+  state = {
+    x: 10
+  };
+
+  render() {
+    return (
+      <div>
+        test
+        <hr />
+        {this.state.x}
+        <hr />
+        <button
+          onClick={() => {
+            this.setState({ x: ++this.state.x });
+          }}
+        >
+          count +
+        </button>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
